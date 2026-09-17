90a682596e22c250d8ec332077935d560fc7cc5e
SammieHuang <95095294+SammieHuang@users.noreply.github.com>
2026-09-16 15:55:15 -1000

    upgrade to expo sdk57 (#21)

--- mobile/services/transcribe-service.ts
+++ mobile/services/transcribe-service.ts
@@ -1,15 +1,11 @@
+import {File} from "expo-file-system"
+
 async function transcribeAudio(uri: string) {
     const formData = new FormData()
     const API_BASE_URL = "http://192.168.4.70:3000";
 
-    formData.append(
-        'audio', 
-        {
-            uri: uri,
-            name: 'recording.m4a',
-            type:'audio/m4a'
-        } as any
-    )
+    const audioFile = new File(uri)
+    formData.append('audio', audioFile, 'recording.m4a')
 
     const response = await fetch(`${API_BASE_URL}/transcribe`, {
         method: 'POST',
